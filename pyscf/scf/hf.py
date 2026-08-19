f9f257382334c2ed5f4a668757fc916b245b9ab2
James Gin <james@orbitalindustries.com>
2026-08-19 12:17:31 +0000

    scf: retain canonical path for non-closed-shell occupations

--- pyscf/scf/hf.py
+++ pyscf/scf/hf.py
@@ -173,7 +173,7 @@ Keyword argument "init_dm" is replaced by "dm0"''')
         last_hf_e = e_tot
 
         fock = mf.get_fock(h1e, s1e, vhf, dm, cycle, mf_diis, fock_last=fock_last)
-        if cycle == 0:
+        if cycle == 0 or not numpy.all((mo_occ == 0) | (mo_occ == 2)):
             mo_energy, mo_coeff = mf.eig(fock, s1e, x=x_orth)
             mo_occ = mf.get_occ(mo_energy, mo_coeff)
         else:
